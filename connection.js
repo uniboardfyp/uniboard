079d128bd8335244fcfac17905c06371fdbe4068
uniboardfyp <129657046+uniboardfyp@users.noreply.github.com>
2023-04-03 11:13:13 +0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 const pool = mysql
   .createPool({
-    host: 'sql12.freemysqlhosting.net',
-    user: 'sql12610458',
-    password: 'JZ8nMlmdYr',
-    database: 'sql12610458',
+    host: 'sql.freedb.tech',
+    user: 'freedb_zuhayr',
+    password: '3xsbGN*HRrP&e&B',
+    database: 'freedb_uniboard',
     dateStrings: true,
   })
   .promise();
