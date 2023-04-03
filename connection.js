9e804238ce24d32b75ffe661235f57722991e034
uniboardfyp <129657046+uniboardfyp@users.noreply.github.com>
2023-04-03 10:48:38 +0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 const pool = mysql
   .createPool({
-    host: 'bbxpz7le1nvyvyu3wdlo-mysql.services.clever-cloud.com',
-    user: 'uiwb5tcqd2aswcg8',
-    password: 'GAWYnKNfWxtE23ug12Jj',
-    database: 'bbxpz7le1nvyvyu3wdlo',
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12610458',
+    password: 'JZ8nMlmdYr',
+    database: 'sql12610458',
     dateStrings: true,
   })
   .promise();
